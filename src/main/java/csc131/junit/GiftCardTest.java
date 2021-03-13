28217a0ab40a9de2fc1b64273e156febe51b8d05
Greg Moua <gregmoua@gmail.com>
2021-03-13 03:12:24 -0800

    Added a GiftCardTest class and made my first Git commit. Hopefully this works well!

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,64 @@
+package csc131.junit;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class GiftCardTest {
+
+	@Test
+	public void getIssuingStore() {
+		double 		balance;
+		GiftCard 	card;
+		int 		issuingStore;
+		
+		issuingStore = 1337;
+		balance 	 = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()",
+					 issuingStore, card.getIssuingStore());
+		
+	}
+	
+	@Test
+	public void getBalance() {
+		double		balance;
+		GiftCard	card;
+		int 		issuingStore;
+		
+		issuingStore = 1337;
+		balance = 500.00;
+		card = new GiftCard(issuingStore, balance);
+		double tolerance = .001;
+		assertEquals("getBalance()", balance, card.getBalance(), .001);
+	}
+	
+	@Test
+	public void deduct_RemainingBalance() {
+		double 		balance;
+		GiftCard 	card;
+		int 		issuingStore;
+		double 		amount;
+		String 		test;
+
+		
+		amount 		 = 20.00;
+		issuingStore = 1337;
+		balance 	 = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		if (amount < 0.0){
+			test = "";
+		}else {
+			if (amount > balance) {
+				test = "Amount Due: ";
+			} else {
+				test = "Remaining Balance: ";
+			}
+			
+			test += String.format("%6.2f", balance-amount);
+		}
+		
+		assertEquals("deduct(amount)", test, card.deduct(amount));
+	}
+
+}
